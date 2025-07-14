2f07c69a409df9b40de3eaa0bbf5dc50d366840f
nakurai <nakuraicodes@gmail.com>
2025-07-14 09:02:46 -0400

    fixed install syntax

--- Formula/gourl.rb
+++ Formula/gourl.rb
@@ -9,13 +9,13 @@ class Gourl < Formula
     if Hardware::CPU.intel?
       url "https://github.com/Nakurai/gourl/releases/download/v#{version}/#{version}-gourl-macos-amd64.zip"
       sha256 "8e1a0468ecdfbcb71d4e5c37f823c05c4dc6896a4d9434b53e05ac4cb80dc6e1"
-      def install do
+      def install
         bin.install "#{version}-gourl-macos-amd64.zip" => "gourl"
       end
     elsif Hardware::CPU.arm?
       url "https://github.com/Nakurai/gourl/releases/download/v#{version}/#{version}-gourl-macos-arm64.zip"
       sha256 "7260f5e874dbec15b59ec467bddcd67f6cf6e15dcaad79c258bd6d13080f9b1c"
-      def install do
+      def install
         bin.install "#{version}-gourl-macos-arm64.zip" => "gourl"
       end
     end
@@ -25,14 +25,14 @@ class Gourl < Formula
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
         url "https://github.com/Nakurai/gourl/releases/download/v#{version}/#{version}-gourl-linux-arm64"
         sha256 "20f35ab819bee1270b57e962ae69cb51621a8d5dbde0c55b3a30b8b6d80158e5"
-        def install do
+        def install
           bin.install "#{version}-gourl-linux-arm64" => "gourl"
         end
     end
     if Hardware::CPU.intel?
         url "https://github.com/Nakurai/gourl/releases/download/v#{version}/#{version}-gourl-linux-amd64"
         sha256 "cc7553bd7d8e2ba5bc2ccdccd96ff272fbd3dba4be432805ad738a13d18f5153"
-        def install do
+        def install
           bin.install "#{version}-gourl-linux-amd64" => "gourl"
         end
     end
