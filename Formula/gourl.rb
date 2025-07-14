f10e42e355e856e63cc1144fb1f6637c8f0fa71d
nakurai <nakuraicodes@gmail.com>
2025-07-14 08:22:01 -0400

    updated gourl to latest version

--- Formula/gourl.rb
+++ Formula/gourl.rb
@@ -3,26 +3,26 @@
 class Gourl < Formula
   desc "A use friendly mix between cURL and tools like Postman, Insomnia, etc."
   homepage "https://github.com/nakurai/gourl"
-  version "1.0.0-alpha"
+  version "1.0.1-alpha"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/Nakurai/gourl/releases/download/v1.0.0-alpha/1.0.0-alpha-gourl-macos-amd64.zip"
-      sha256 "d62c516ead1628f0f6ac11ec1a20a97218411887ab6e8afaa9621a922149d571"
+      url "https://github.com/Nakurai/gourl/releases/download/v#{version}/#{version}-gourl-macos-amd64.zip"
+      sha256 "8e1a0468ecdfbcb71d4e5c37f823c05c4dc6896a4d9434b53e05ac4cb80dc6e1"
     elsif Hardware::CPU.arm?
-      url "https://github.com/Nakurai/gourl/releases/download/v1.0.0-alpha/1.0.0-alpha-gourl-macos-arm64.zip"
-      sha256 "5396a0ae1d781446b020019492a5168462aed949ca93f34269b2e47ec621162d"
+      url "https://github.com/Nakurai/gourl/releases/download/v#{version}/#{version}-gourl-macos-arm64.zip"
+      sha256 "7260f5e874dbec15b59ec467bddcd67f6cf6e15dcaad79c258bd6d13080f9b1c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-        url "https://github.com/Nakurai/gourl/releases/download/v1.0.0-alpha/1.0.0-alpha-gourl-linux-arm64"
-        sha256 "6d9adebf712fab49f7471d86e3f905d220a092f27299c67c997b7dd647207955"
+        url "https://github.com/Nakurai/gourl/releases/download/v#{version}/#{version}-gourl-linux-arm64"
+        sha256 "20f35ab819bee1270b57e962ae69cb51621a8d5dbde0c55b3a30b8b6d80158e5"
     end
     if Hardware::CPU.intel?
-        url "https://github.com/Nakurai/gourl/releases/download/v1.0.0-alpha/1.0.0-alpha-gourl-linux-amd64"
-        sha256 "6d9adebf712fab49f7471d86e3f905d220a092f27299c67c997b7dd647207955"
+        url "https://github.com/Nakurai/gourl/releases/download/v#{version}/#{version}-gourl-linux-amd64"
+        sha256 "cc7553bd7d8e2ba5bc2ccdccd96ff272fbd3dba4be432805ad738a13d18f5153"
     end
   end
 
